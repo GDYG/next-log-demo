ad4c0b23be06658551952e097c86379810defad5
GDYG <1549472324@qq.com>
2024-04-01 16:07:19 +0800

    feat: test app api request

--- app/home/components/HomeChild.tsx
+++ app/home/components/HomeChild.tsx
@@ -1,3 +1,7 @@
-export default function HomeChild() {
+export default async function HomeChild() {
+  // const data = await fetch("http://localhost:3000/api/log/dc/test").then(
+  //   (res) => res.json()
+  // );
+
   return <section>HomeChild Page</section>;
 }
